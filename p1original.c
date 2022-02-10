cb52939c2edf25b2179cbe21e6d06a17c17027a2
Meidingu <94587606+Meidingu@users.noreply.github.com>
2022-02-10 03:30:52 +0000

    no

--- p1original.c
+++ p1original.c
@@ -1,2 +1,29 @@
+#include<stdio.h>
+#include<math.h>
 
+void input(float *base,float *height)
+{
+  printf("Enter the value of base =\n");
+  scanf("%f",base);
+  printf("Enter the value of height =\n");
+  scanf("%f",height);
+}
+
+void area(float base,float height,float *area)
+{
+  *area = ((base*height)/2);
+}
+
+void output(float base,float height,float area)
+{
+  printf("The area of triange is = %f\n",area);
+}
+
+int main()
+{
+  float b,h,a;
+  input(&b,&h);
+  area(b,h,&a);
+  output(b,h,a);
+  return 0;
 }
